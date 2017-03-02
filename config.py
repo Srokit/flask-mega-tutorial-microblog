ab7e52a416e09f0288ea73f65c754f3f56113767
Stan Rokita <srokita@umich.edu>
2017-03-02 11:15:59 -0500

    Show openID providers as options for login

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
+OPENID_PROVIDERS = [
+    {
+        'url': "https://www.myopenid.com",
+        'name': "MyOpenID"
+    },
+    {
+        'url': "http://www.openid.aol.com/<username>",
+        'name': "AOL"
+    }
+]
