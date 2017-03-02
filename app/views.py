4494e5e8a98f4773bb3ab82fc94f03956d37bd9c
Stan Rokita <srokita@umich.edu>
2017-03-02 09:56:11 -0500

    First template example with /index route

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    # Placeholder user
+    user = {'nickname': 'Miguel'}
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
