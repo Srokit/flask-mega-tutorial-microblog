06fdc94a6d5fa003038475d1d697542aa68aefb9
Stan Rokita <srokita@umich.edu>
2017-03-02 14:36:29 -0500

    Got a functioning login system working!

--- config.py
+++ config.py
@@ -8,11 +8,7 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 OPENID_PROVIDERS = [
     {
-        'url': "https://www.myopenid.com",
-        'name': "MyOpenID"
-    },
-    {
-        'url': "http://www.openid.aol.com/<username>",
-        'name': "AOL"
+        'url': "http://me.yahoo.com",
+        'name': "Yahoo"
     }
 ]
